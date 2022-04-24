b08d3a1ed6c01b6f06e8064c4f039351e4af6c7e
Yhonka Machado <yhonka.yjml@gmail.com>
2022-04-24 14:38:53 -0400

    My first

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import Logo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics+1);
+    
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(numClics*0);
+  };
+
+  return (
+    <div className="App">
+      <div className='logo-contenedor'>
+        <img 
+          className='logo'
+          src={Logo}
+          alt='Logo'
+        />
+      </div>
+
+      <div className='contenedor-principal'>
+
+          <Contador numClics={numClics}/>
+
+          <Boton
+            texto='Clic'
+            esBotonDeClic={true}
+            manejarClic={manejarClic}
+          />
+
+          <Boton
+            texto='Reiniciar'
+            esBotonDeClic={false}
+            manejarClic={reiniciarContador}
+          />
+      </div>
+    </div>
+  );
+}
+
+export default App;
