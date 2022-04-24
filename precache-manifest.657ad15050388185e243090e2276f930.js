6de0e37d93b8e989a381f40537a79ab10fec3b0b
Yhonka Machado <yhonka.yjml@gmail.com>
2022-04-24 15:22:31 -0400

    Updates

---
+++ precache-manifest.657ad15050388185e243090e2276f930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "743a84b4514db51cc727",
+    "url": "/ContadorDeClic/static/css/main.6fe869ee.chunk.css"
+  },
+  {
+    "revision": "743a84b4514db51cc727",
+    "url": "/ContadorDeClic/static/js/main.743a84b4.chunk.js"
+  },
+  {
+    "revision": "3d978e9a755fb10b1846",
+    "url": "/ContadorDeClic/static/js/1.3d978e9a.chunk.js"
+  },
+  {
+    "revision": "6ca8509bf1a6329bb4f6",
+    "url": "/ContadorDeClic/static/js/runtime~main.6ca8509b.js"
+  },
+  {
+    "revision": "c00d94842ae093a74ce52becdeacb98b",
+    "url": "/ContadorDeClic/static/media/freecodecamp-logo.c00d9484.png"
+  },
+  {
+    "revision": "4749b040dea8fae1c0ce2a23e1ef4013",
+    "url": "/ContadorDeClic/index.html"
+  }
+];
